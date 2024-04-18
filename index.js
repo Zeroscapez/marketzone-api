760451f0b6900c7c1cbf0817240fa5fa9948aeb5
Zeroscapez <ultlomini@gmail.com>
2024-04-18 03:15:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'busterswordisbae@gmail.com',
-    pass: 'OrientalFlower13',
+    pass: 'gyxz atjb tjvi oime',
   },
 });
 
